ec88b8968f4cc401d0597abf5113c5c413ce21ef
Arthur <truong.nguyen2@sotatek.com>
2023-01-09 16:02:52 +0700

    Add verfication helper function

--- deploy/FarmBoosterProxy.deploy.ts
+++ deploy/FarmBoosterProxy.deploy.ts
@@ -1,19 +1,20 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
 
 const deployFarmBoosterProxy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const {deployments, getNamedAccounts, ethers} = hre;
-  const {deploy} = deployments;
-  const {deployer} = await getNamedAccounts();
+  const { deployments, getNamedAccounts, ethers } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
 
   await deploy('FarmBoosterProxy', {
     from: deployer,
     args: [],
     log: true,
-    deterministicDeployment: false
-  });
-};
+    deterministicDeployment: false,
+  })
+}
 
-deployFarmBoosterProxy.tags = ["BOOSTER_PROXY"];
+deployFarmBoosterProxy.tags = ['BOOSTER_PROXY']
+deployFarmBoosterProxy.skip = () => Promise.resolve(true)
 
-export default deployFarmBoosterProxy;
+export default deployFarmBoosterProxy
---
+++ helpers/misc-utils.ts
@@ -0,0 +1,3 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+export let DRE: HardhatRuntimeEnvironment
